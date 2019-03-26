96badf5d18f19758a3ae34206f530710e8e6e45d
liuriji <liurj@weipaitang.com>
2019-03-26 13:41:09 +0800

    xx

---
+++ page/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+arr=("0archives.html"
+    "1category.html"
+    "2tags.html"
+    "3collections.md"
+    "4about.md")
+file=${arr[$1]}
+
+if [ $file ];
+then
+    git pull
+    git add $file
+    git commit -m "file"
+    git push origin master
+else
+    echo "error"
+fi
+
